fc535867a0e24b9e1fc4f57f9217f22d5918d6fa
Ubuntu <ubuntu@ip-172-31-15-118.ap-northeast-2.compute.internal>
2022-11-14 14:04:12 +0000

    수정중

--- threads/thread.c
+++ threads/thread.c
@@ -248,7 +248,8 @@ thread_unblock (struct thread *t) {
 	t->status = THREAD_READY;
 	intr_set_level (old_level);
 
-	if (t->priority > thread_current()->priority)
+	if (t != initial_thread && 
+		t->priority > thread_current()->priority)
 		thread_yield();
 
 }
