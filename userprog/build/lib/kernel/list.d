0f435234fc012d34037e7e8be8ea3d961ff195ab
Ubuntu <ubuntu@ip-172-31-15-118.ap-northeast-2.compute.internal>
2022-11-21 18:26:55 +0000

    feat: syscall_exit 임시구현완료

---
+++ userprog/build/lib/kernel/list.d
@@ -0,0 +1,5 @@
+lib/kernel/list.o: ../../lib/kernel/list.c \
+ ../../include/lib/kernel/list.h ../../include/lib/stdbool.h \
+ ../../include/lib/stddef.h ../../include/lib/stdint.h \
+ ../../include/threads/thread.h ../../include/lib/debug.h \
+ ../../include/threads/interrupt.h ../../include/lib/kernel/../debug.h
